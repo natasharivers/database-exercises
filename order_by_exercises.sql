b79b343d5c52da28b7083295251328bb2568f992
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-03-25 15:36:58 -0500

    Update order_by_exercises.sql

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,7 +4,7 @@
 select first_name, last_name
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
-	order by first_name,
+	order by first_name;
 
 -- #3 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. (709 returns) 
 -- What was the first and last name in the first row of the results? Irena Acton
@@ -28,7 +28,7 @@ where first_name in ('Irena', 'Vidya', 'Maya')
 -- the last employee number with their first and last name (499648- Erde, Tadahiro)
 select last_name, first_name, emp_no
 from employees
-where last_name like 'e%'and last_name like '%e'
+where last_name like 'e%e'
 	order by emp_no;
 
 -- #6 Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. 
@@ -37,14 +37,19 @@ where last_name like 'e%'and last_name like '%e'
 -- the name of the oldest emmployee. (Sergi Erde in 1985-02-02)
 select first_name, last_name, hire_date
 from employees
-where last_name like 'e%' and last_name like '%e'
+where last_name like 'e%e' 
 	order by hire_date DESC;
 
 -- #7 Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. 
 -- the number of employees returned (362 returns)
 -- the name of the oldest employee who was hired last (Khun Bernini)
--- the name of the youngest emmployee who was hired first (Douadi Pettis)
 select first_name, last_name, hire_date, birth_date 
 from employees
 where hire_date like '199%' and birth_date like '%-12-25'
 	order by birth_date , hire_date DESC;
+
+-- the name of the youngest emmployee who was hired first (Douadi Pettis)
+select first_name, last_name, hire_date, birth_date 
+from employees
+where hire_date like '199%' and birth_date like '%-12-25'
+	order by birth_date Desc , hire_date;
